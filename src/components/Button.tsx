50ee7078fde2f59764e3c31715c2aa30aa199992
Zack Stickles <zstickles@gmail.com>
2021-01-06 15:44:01 -0800

    feat: basic button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,8 @@ import Icon, { ICON_TYPES } from './Icon';
 
 interface IButtonProps {
   theme: INamesAppTheme
-  onclick?: () => void; // TODO?
+  noBorder?: boolean;
+  onclick?: () => void;
 }
 
 interface IWithIcon extends IButtonProps {
@@ -18,12 +19,52 @@ interface IWithText extends IButtonProps {
   icon?: keyof typeof ICON_TYPES;
 }
 
-// TODO: styling
-const Button: React.FC<IWithText | IWithIcon> = ({ theme, icon, children }) => (
-  <div role="button">
+const Button: React.FC<IWithText | IWithIcon> = ({ theme, noBorder, icon, onclick, children }) => (
+  <button
+    role="button"
+    onClick={() => onclick?.()}
+    css={css`
+      /* TODO: colors via prop with a default */
+      --color-border: #ccc;
+      --color-background: #ddd;
+      --color-font: ${theme.color.text.primary};
+
+      vertical-align: middle;
+      display: inline-flex;
+      align-items: center;
+      cursor: pointer;
+      margin: 0 0.5em;
+      padding: 0.5em;
+      position: relative;
+      user-select: none;
+      outline: none;
+      font: ${theme.fonts.primary};
+      color: var(--color-font);
+      background-color: var(--color-background);
+      border-radius: ${children ? '0.75em' : '1.5em'};
+
+      svg + span {
+        margin-left: 0.5em;
+      }
+
+      ${!noBorder && css`
+        border: 2px solid;
+        border-bottom-width: 4px;
+        border-color: var(--color-border);
+
+        &:active {
+          margin-top: 2px;
+          transform: translateY(2px);
+          border-bottom-width: 2px;
+        }
+      `}
+
+      ${noBorder && 'border: none;'}
+    `}
+  >
     {icon && <Icon name={icon} />}
-    {children}
-  </div>
+    {children && <span>{children}</span>}
+  </button>
 );
 
 export default withTheme(Button);
--- src/components/Mock.tsx
+++ src/components/Mock.tsx
@@ -7,10 +7,17 @@ const Mock: React.FC<{ theme: INamesAppTheme }> = ({ theme }) => (
   <div
     css={css`
       color: ${theme.color.text.primary};
+      padding: 20px;
     `}
   >
     Mock screen goes here.
-    <Button icon="settings">settings</Button>
+    <Button icon="settings">Settings</Button>
+    <br />
+    And also perhaps it goes here, I'm not really too sure.
+    <Button icon="settings" />
+    <p>
+      <Button noBorder>What?</Button>
+    </p>
   </div>
 );
 
